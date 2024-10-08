83ebe2f8a8fdf2953fbd735f7a8641c01d0d59c7
Afroz Khan <Afrozaftab11@gmail.com>
2024-10-08 06:26:07 +0500

    Day 18,Challenge has been Completed.

--- 100_Days_Code_Challenge/Day_19/question_55.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Question 55: Double Numbers in an Array: Make a list of numbers. Then, use a trick to make a
-//  new list where each number is twice its original value.    
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let doubleNumbers = numbers.map(number => number * 2);
-console.log(doubleNumbers);
--- 100_Days_Code_Challenge/Day_19/question_57.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Question 57: Find the Average Grade: Given a list of grades, calculate the average grade.
-let grades = [56, 78, 96, 86, 89, 68];
-let total = grades.reduce((total, grade) => total + grade, 0);
-let average_grade = total / grades.length;
-console.log(average_grade);
