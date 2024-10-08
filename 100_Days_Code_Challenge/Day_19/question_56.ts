b0b0601afeb68b9acfa2a4f5ff4b4a3592ff528e
Afroz Khan <Afrozaftab11@gmail.com>
2024-10-08 06:25:40 +0500

    Day 19,Challenge has been Completed.

---
+++ 100_Days_Code_Challenge/Day_19/question_56.ts
@@ -0,0 +1,10 @@
+// Question 56: Keep Only Strings: Given a mix of different types of items, make a new list that has only the words.
+
+
+
+
+let mixedArray:(string | number | boolean )[] = ["coding", "error", 0 , true,"TypeScript"];
+
+let stringArray = mixedArray.filter(name => typeof name === "string");
+
+console.log(stringArray);
