ee65afec4cf91c1c8b55d33d789f94ba31a2969a
Afroz Khan <Afrozaftab11@gmail.com>
2024-11-14 11:36:03 +0500

    Day 26,Challenge has been completed

---
+++ 100_Days_Code_Challenge/Day_26/question_77.js
@@ -0,0 +1,8 @@
+"use strict";
+// Question 77: Default Parameters: Write a function that greets a user. It should take the user's 
+// name as a parameter and say hello. If no name is given, it should greet an anonymous user.
+function greetUser(name = "anonymous") {
+    console.log(`Hello, ${name}!`);
+}
+greetUser("Murad");
+greetUser();
